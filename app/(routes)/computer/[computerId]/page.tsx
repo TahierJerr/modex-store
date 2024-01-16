4154d21e274ce7dfa124373a152be66b9f9140bd
Taher Jerjissi <128802571+TahierJerr@users.noreply.github.com>
2024-01-16 20:24:47 +0100

    update filter

--- app/(routes)/computer/[computerId]/page.tsx
+++ app/(routes)/computer/[computerId]/page.tsx
@@ -10,7 +10,7 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
     title: 'MODEX',
-  }
+}
 
 interface ComputerPageProps {
     params: {
@@ -23,31 +23,29 @@ const ComputerPage: React.FC<ComputerPageProps> = async ({ params }) => {
     const otherComputers = await getComputers({
         categoryId: computer?.category?.id
     });
- 
+
     const filteredComputers = otherComputers.filter(
-        (item) => item.id !== computer.id
+        (item) => item.id > computer.id && item.id < computer.id && item.id !== computer.id
     );
 
-    
- 
     return (
         <div className="bg-black text-white mt-16 md:mt-0">
             <Container>
                 <div className="px-4 py-10 sm:px-6 lg:px-8">
-                   <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-                       <Gallery images={computer.images}/>
-                       <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-                       <ComputerInfo data={computer} />
-                       </div>
-                   </div>
-                   <hr className="my-10 text-black200" />
-                   <ComputerSpec data={computer} />
-                   <hr className="my-10 text-black200" />
-                   <ComputerList title="Andere Computers" description="" items={filteredComputers} />
+                    <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+                        <Gallery images={computer.images} />
+                        <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+                            <ComputerInfo data={computer} />
+                        </div>
+                    </div>
+                    <hr className="my-10 text-black200" />
+                    <ComputerSpec data={computer} />
+                    <hr className="my-10 text-black200" />
+                    <ComputerList title="Andere Computers" description="" items={filteredComputers} />
                 </div>
             </Container>
         </div>
     )
- }
+}
 
 export default ComputerPage;
