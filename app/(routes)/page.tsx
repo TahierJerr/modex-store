4b7e241182ad0c37b28fe707012c69496cecdbc0
Taher Jerjissi <128802571+TahierJerr@users.noreply.github.com>
2024-01-18 11:59:50 +0100

    added lazy loading to home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -2,27 +2,31 @@ import Billboard from "@/components/billboard";
 import getBillboard from "@/actions/get-billboard";
 import Container from "@/components/ui/container";
 import getComputers from "@/actions/get-computers";
-import ComputerList from "@/components/computer-list";
 import type { Metadata } from 'next'
+import dynamic from "next/dynamic";
 
 export const revalidate = 0;
 
 export const metadata: Metadata = {
     title: 'MODEX Prebuilt Gaming PCs | MODEX',
-  }
+}
+
+const ComputerList = dynamic(
+    () => import("@/components/computer-list"),
+);
 
 const HomePage = async () => {
     const computers = await getComputers({ isFeatured: true });
     const billboard = await getBillboard("5854fd05-b077-4185-b72e-16539570c641");
     return (
         <div className="space-y-10 pb-10">
-                <Container>
+            <Container>
                 <Billboard data={billboard} />
                 <div className="flex flex-col gap-2 px-4 sm:px-6 lg:px-8">
                     <ComputerList description="Check onze MODEX Pre-Builds" title="MODEX PCs" items={computers} />
                 </div>
-        </Container>
-            </div>
+            </Container>
+        </div>
     )
 }
 
