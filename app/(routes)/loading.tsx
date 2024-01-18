361bc028b04fab4c3f54292ef77b220a310f89c0
Taher Jerjissi <128802571+TahierJerr@users.noreply.github.com>
2024-01-18 18:32:52 +0100

    push

--- app/(routes)/loading.tsx
+++ app/(routes)/loading.tsx
@@ -1,21 +1,38 @@
-import Image from 'next/image'
+"use client";
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 export default function Loading() {
+    const [showContent, setShowContent] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowContent(true);
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black">
-            <div className="max-w-screen-sm max-h-screen-sm text-center">
-                <Image
-                    src="/path/to/loading-logo.png"
-                    alt="Loading Logo"
-                    width={300}
-                    height={300}
-                    quality={100}
-                    className="mx-auto"
-                    priority={true}
-                    fetchPriority='high'
-                />
-                <p className="text-primary font-semibold">Loading<span className="loading-dot">...</span></p>
-            </div>
+            {showContent ? (
+                <div className="max-w-screen-sm max-h-screen-sm text-center">
+                    <Image
+                        src="/path/to/loading-logo.png"
+                        alt="Loading Logo"
+                        width={300}
+                        height={300}
+                        quality={100}
+                        className="mx-auto"
+                        priority={true}
+                        fetchPriority="high"
+                    />
+                    <p className="text-primary font-semibold">
+                        Loading<span className="loading-dot">...</span>
+                    </p>
+                </div>
+            ) : null}
         </div>
-    )
+    );
 }
