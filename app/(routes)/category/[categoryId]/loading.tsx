caa5c48fda6dd2b3b3c6423e6b1fed1ff5662fb8
Taher Jerjissi <128802571+TahierJerr@users.noreply.github.com>
2024-01-19 15:03:26 +0100

    loading component

---
+++ app/(routes)/category/[categoryId]/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingNow from "@/components/loading"
+
+export default function Loading() {
+    return (
+        <div className="flex items-center justify-center">
+            <LoadingNow />
+        </div>
+    )
+}
---
+++ components/loading.tsx
@@ -0,0 +1,7 @@
+const LoadingNow = () => {
+    return (
+        <p className="text-lg font-semibold text-primary md:text-xl">Loading...</p>
+    )
+}
+
+export default LoadingNow;
