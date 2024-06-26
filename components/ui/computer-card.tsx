940a01e7702e41006865e49e1309942c0d340e45
Taher Jerjissi <128802571+TahierJerr@users.noreply.github.com>
2024-06-26 12:00:03 +0200

    computer card change

--- components/ui/computer-card.tsx
+++ components/ui/computer-card.tsx
@@ -3,7 +3,7 @@
 import { Computer } from "@/types";
 
 import Image from "next/image";
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, ShoppingCartIcon } from "lucide-react";
 
 import Button from "@/components/ui/button";
 import { useRouter } from "next/navigation";
@@ -21,34 +21,30 @@ const Card: React.FC<CardProps> = ({ data }) => {
     };
     
     return (
-    <div onClick={handleClick} className="bg-white rounded-lg shadow-xl text-black flex-col flex cursor-pointer transition-all duration-300 hover:bg-slate">
+    <div onClick={handleClick} className="bg-white rounded-lg drop-shadow-lg shadow-xl text-black flex-col flex cursor-pointer transition-all duration-300 hover:bg-slate border">
         <div className="w-full min-h-80 aspect-square rounded-t-md flex items-center">
             {data?.images?.[0]?.url ? (
                 <div className="relative">
                     <Image src={data?.images?.[0]?.url} width={1000} height={1000} quality={100} alt={data.name} loading="lazy" className="rounded-md" />
-                    <div className="bg-black text-white flex items-center justify-center bottom-0 w-full absolute py-2 px-4 font-semibold">Verbeterde foto{"\'"}s komen binnenkort!</div>
                 </div>
                 ) : (
                 <p className="w-full h-auto text-center text-gray">Geen afbeelding beschikbaar.</p>
                 )}
             </div>
-            <div className="p-4 flex flex-col">
+            <div className="py-2 flex flex-col justify-center text-center">
                 <p className="font-bold text-xl ">{data.name}</p>
-                <p className="text-lg font-semibold">€ {data.price}</p>
                 <hr className="mt-2 " />
                 <div className="mt-2">
                     <p className="">{data.graphics.model}</p>
                     <p className="">{data.processor.name}</p>
                     <p className="">{data.memory.name}</p>
                 </div>
-                <hr className="mt-2 text-black100" />
-                <div className="flex items-center mt-4">
-                    <Button onClick={handleClick} className="px-4 py-2 flex text-md items-center w-full justify-center rounded-lg hover:opacity-75 transition-all bg-black text-white ">
-                        Ontdek!
-                        <ChevronRightIcon size={20} className="ml-1" />
-                    </Button>
-                </div>
+                <hr className="my-2 text-black100" />
+                <p className="text-lg font-semibold">€ {data.price}</p>
             </div>
+            <Button onClick={handleClick} className="py-2 rounded-b-lg flex rounded-t-none hover:bg-blue-950 hover:transition-all justify-center items-center text-white bg-black w-full text-center">
+                <ShoppingCartIcon size={20} className="mr-2" /> Add to cart
+            </Button>
         </div>
         );
     };
