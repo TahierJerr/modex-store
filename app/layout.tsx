38338c512a09b32a0449ba7adcc7449a6eb5d22b
Taher Jerjissi <128802571+TahierJerr@users.noreply.github.com>
2024-09-16 19:14:37 +0200

    seo improvments

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
         <Footer />
         <CookieConsentComponent />
         <GoogleAnalytics gaId='G-Y8B38HERJE' />
+        <GoogleAnalytics gaId='G-JNT40MYQTZ' />
     </body>
     </html>
     </>
